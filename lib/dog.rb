627fd663eafd1bf4c758f348ee9282c08a52c3c2
melia miller <meliajm@gmail.com>
2019-12-12 05:49:08 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,17 @@
 # Your code goes here!
+
+
+class Dog 
+  
+  def name=(dog_name)
+    @dogs_name = dog_name
+  end 
+  
+  def name
+    @dogs_name
+  end
+  
+  def bark
+    puts "woof!"
+  end 
+end
